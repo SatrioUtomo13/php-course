01a46a17c27e392d626e0afe00dd3a2032a807e7
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2022-03-03 13:57:22 +0700

    add variable and null

---
+++ null.php
@@ -0,0 +1,8 @@
+<?php 
+    //create null variable
+    $name = 'satrio';
+    $name = null;
+
+    //checking variable using is null
+    var_dump(is_null($name));
+?>
--- variable.php
+++ variable.php
@@ -1,5 +1,5 @@
 <?php 
-
+    //regular variable
     $name = 'satrio';
     $age = 23;
 
@@ -9,6 +9,23 @@
 
     echo 'age : ';
     echo $age;
+    echo '<br>';
 
+    //delete variable using unset
+    // $try = 'this is try';
+    // unset($try);
+    // echo $try;
 
+    //checking variable using isset
+    var_dump(isset($name));
+    echo '<br>';
+
+    //study case : checking variable 
+
+    if(isset($name) === true){
+        echo 'welcome ';
+        echo $name;
+    }else{
+        echo 'data is not exist';
+    }
 ?>
