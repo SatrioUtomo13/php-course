b0a7f5cd747072c93c48a881cd984ee7e08bdcfb
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2022-03-03 15:12:02 +0700

    aritmatika

---
+++ aritmatika.php
@@ -0,0 +1,32 @@
+<?php 
+
+    //increament
+    $variable1 = 10;
+    $variable2 = 20;
+    $add = $variable1 + $variable2;
+    var_dump($add);
+
+    echo '<br>';
+
+    //decreament
+    $dec = $variable1 - $variable2;
+    var_dump($dec);
+
+    echo '<br>';
+
+    //multiple
+    $multi = $variable1 * $variable2;
+    echo $multi;
+
+    echo '<br>';
+
+    //division
+    $division = $variable1 / $variable2;
+    echo $division;
+
+    echo '<br>';
+
+    //modulo
+    $modulo = $variable2 % $variable1;
+    echo $modulo;
+?>
