21f39eec449a93c1ab1f6cceb9643b37ae8ef6df
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2022-03-09 10:39:10 +0700

    manipulasi string

--- manipulasiString.php
+++ manipulasiString.php
@@ -3,5 +3,39 @@
     // combine string using dot .
 
     $name = 'satrio';  
-    echo 'name : ' . $name . PHP_EOL;
+    echo 'name : ' . $name;
+
+    echo "<br>";
+    echo "<br>";
+
+    //convert 
+    $vstring = (string)100;
+    var_dump($vstring);
+
+    echo "<br>";
+
+    $vint = (int)'100';
+    var_dump($vint);
+
+    echo "<br>";
+
+    $vfloat = (float)'1.234';
+    var_dump($vfloat);
+
+    echo "<br>";
+    echo "<br>";
+
+    //acces variable
+
+    echo $name[2];
+    // echo $name[6]; //this is error
+
+    echo "<br>";
+    echo "<br>";
+
+    //variable parsing : seems like combine string using dot we've discuss before
+    //using double qoute ("") or heredoc (<<<)
+    echo "hello $name , welcome";
+    echo "<br>";
+    echo "hello {$name}s, welcome";
 ?>
