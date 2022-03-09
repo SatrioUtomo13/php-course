13aff39a243d2e501f25a04710907bd5ee407ca3
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2022-03-09 11:35:41 +0700

    study case ternary operator

--- ternaryOperator.php
+++ ternaryOperator.php
@@ -2,7 +2,14 @@
 
 $gender = 'wanita';
 $hi = $gender == 'pria' ? 'Hi bro' : 'Hi sist';
-
 echo $hi;
+
+echo '<br>';
+
+//study case : confirm age
+
+$age = 17;
+$confirm = $age >= 18 ? 'you can enter' : 'too young';
+echo $confirm;
 
 ?>
