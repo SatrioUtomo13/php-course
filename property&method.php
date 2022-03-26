eaf35c7ea739481032958141b8969197076bb26a
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2022-03-26 16:59:57 +0700

    property and method

--- property&method.php
+++ property&method.php
@@ -17,15 +17,41 @@
         
         public function sayHello(){
             return "Hi $this->name";
-        
+        }
+
+        public function advantage(){
+            switch ($this->currency) {
+                case "Rupiah":
+                    return "your profit is Rp.$this->adv";
+                    break;
+                
+                default : 
+                    return "your Profit is sdfsf $this->adv";
+                    break;
+            }
         }
     }
-    $user1 = new Users();
-    $user1->name = "satrio";
-    $user1->id = "01";
-    $user1->hp = "08881227665";
+    $buyer = new Users();
+    $buyer->name = "satrio";
+    $buyer->id = "01";
+    $buyer->hp = "08881227665";
+    $buyer->bank = "BCA";
+    $buyer->address = "jakarta-indonesia";
+
+    $seller = new Users();
+    $seller->name = "utomo";
+    $seller->id = "02";
+    $seller->hp = "0853212023";
+    $seller->bank = "BRI";
+    $seller->currency = "Rupiah";
+    $seller->adv = 0;
     
-    echo $user1->sayHello() . ",welcome back";
+    echo $buyer->sayHello() . ",welcome back";
+
+    echo "<br>";
+    echo "<br>";
+
+    echo $seller->advantage();
     
 
 ?>
