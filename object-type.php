2237734b596f938740bd63d92c097d74cb6d725e
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2022-06-11 14:07:56 +0700

    object type

---
+++ object-type.php
@@ -0,0 +1,36 @@
+<?php
+/* 
+video ke 3: object type
+digunakan agar dapat sebuah object dapat dijadikan type data
+*/
+class mobil {
+    public $merk, $warna, $harga;
+
+    public function __construct($merkMobil, $warnaMobil, $hargaMobil){
+        $this->merk = $merkMobil;
+        $this->warna = $warnaMobil;
+        $this->harga = $hargaMobil;
+    }
+
+    public function getLabel(){
+        return "$this->merk , $this->warna , $this->harga";
+    }
+
+    
+};
+
+class getInfoProduct {
+    public function cetak (mobil $mobil){
+        $str = "{$mobil->merk} & {$mobil->warna} & {$mobil->harga}";
+        return $str;
+    }
+}
+
+
+$produk = new mobil("Toyota", "merah", 10000);
+
+//instansiasi object menggunakan kelas yang dibuat lalu panggil function beserta object mana yang ingin ditampilkan
+$infoProduk = new getInfoProduct();
+echo $infoProduk->cetak($produk);
+
+?>
